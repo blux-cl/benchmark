3473913d13536cccaf5342371b50200f09a640f7
victorvargass <victorvargassandoval93@gmail.com>
2023-03-04 02:09:58 -0300

    Unify jsons by splits script

--- UniChem/unify_generated_json.py
+++ UniChem/unify_generated_json.py
@@ -1,15 +1,29 @@
 import json
 import pandas as pd
 from pathlib import Path
+import logging
+
+logging.basicConfig(filename='unify.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S', level=logging.DEBUG)
 
 pathlist = Path('generated_json/').rglob('*.json')
 data = []
-
-for file in pathlist:
+i = 0
+for n, file in enumerate(pathlist):
     with open(file) as f:
         file_data = json.load(f)
         file_data['uci'] = file.stem
+    logging.info(f"{n} - {str(file)} processed")
     data.append(file_data)
 
-with open(f"compounds.json", 'w') as fp:
+    if n % 20000 == 0 and n != 0:
+        with open(f"compounds_{i}_{n}.json", 'w') as fp:
+            json.dump(data, fp)
+        data = []
+        i = n + 1
+
+i = i + 20000
+with open(f"compounds_{i}_{n}.json", 'w') as fp:
     json.dump(data, fp)
