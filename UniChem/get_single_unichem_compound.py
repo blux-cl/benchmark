2edd3087ba253a0f489a5b9aba10b22a7a63d5e9
victorvargass <victorvargassandoval93@gmail.com>
2023-03-04 02:07:37 -0300

    Unnecesary code removed

--- UniChem/get_single_unichem_compound.py
+++ UniChem/get_single_unichem_compound.py
@@ -19,7 +19,12 @@ headers = {"Content-Type": "application/json"}
 correct, incorrect = [], []
 df = pd.DataFrame([], columns=sources)
 #idx_list = [10000, 20000, 33869, 55513, 55514, 55515, 80000, 124764]
-idx_list = [160000]
+idx_list = [ 
+            634657, 439827, 341878, 1101956, 653050, 153903, 580448,
+            156063, 404393, 27304621, 310642, 383905, 1070662,
+            165270508, 63796124, 1098473, 49827300, 22766067,
+            98849321, 26619068
+            ]
 
 for unichem_id in tqdm(idx_list):
     data = {
@@ -45,31 +50,3 @@ for unichem_id in tqdm(idx_list):
             incorrect.append(unichem_id)
     except Exception as e:
         incorrect.append(unichem_id)
-
-# +
-compound_name = "thymol" #"thymol", "putrescine", "carboxymethylcellulose"
-data = {
-  "compound": compound_name,
-  "type": "name"
-}
-response = requests.post(url, json=data, headers = headers)
-res = json.loads(response.text)
-compounds = res['compounds']
-if res['response'] != 'Not found':
-    sources_dict = dict.fromkeys(sources, '')
-    sources_dict['uci'] = compounds[0]['uci']
-    for c in compounds:
-        compound_sources = c['sources']
-        for s in compound_sources:
-            sources_dict[s['shortName']] = s['compoundId']
-else:
-    sources_dict = {}
-    
-sources_dict
-# -
-
-df_dictionary = pd.DataFrame([sources_dict])
-df = pd.concat([df, df_dictionary], ignore_index=True)
-df.to_csv(f'{compound_name}.csv', index=None)
-
-
